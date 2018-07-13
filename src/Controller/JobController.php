531092bf6959d8a7e5bbd406ba8034a0ef843456
Gabi Udrescu <gabriel.udr@gmail.com>
2018-07-13 18:41:07 +0300

    new job with upload poc

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Job;
+use App\Form\JobType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
@@ -35,5 +39,50 @@ class JobController extends Controller
         return $this->render('job/show.html.twig', [
             'job' => $job,
         ]);
+    }
+
+    /**
+     * @Route("/job/new", name="job_new")
+     * @Method({"GET","POST"})
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $job = new Job();
+
+        $form = $this->createJobType($job);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($job);
+            $em->flush();
+
+            return $this->redirectToRoute('job_show', [
+                'id' => $job->getId(),
+                'company' => $job->getCompanySlug(),
+                'location' => $job->getLocationSlug(),
+                'position' => $job->getPositionSlug(),
+            ]);
+        }
+
+        return $this->render('job/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    private function createJobType(Job $job) : FormInterface
+    {
+        $form = $this->createForm(JobType::class, $job, [
+            'action' => $this->generateUrl('job_new'),
+            'method' => 'POST',
+        ]);
+        $form->add('submit', SubmitType::class);
+
+        return $form;
     }
 }
---
+++ src/Form/JobType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class JobType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('category')
+            ->add('type', ChoiceType::class, [
+                'choices' => [
+                    'Full Time' => 'full-time',
+                    'Part Time' => 'part-time',
+                    'Freelance' => 'freelance',
+                ],
+                'placeholder' => 'jobeet.category'
+            ])
+            ->add('company')
+            ->add('logoFile', VichImageType::class)
+            ->add('url', UrlType::class)
+            ->add('position')
+            ->add('location')
+            ->add('description', CKEditorType::class)
+            ->add('howToApply', CKEditorType::class)
+            ->add('email', EmailType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Job::class,
+        ]);
+    }
+}
