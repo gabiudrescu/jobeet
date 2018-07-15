6d466d4c31e0639ea0066e0989a97ffca799b2a8
Gabi Udrescu <gabriel.udr@gmail.com>
2018-07-15 20:05:23 +0000

    fix docker

--- docker/app/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-groupadd -g "$APPLICATION_GID" "$APPLICATION_GROUP"
-useradd -u "$APPLICATION_UID" --home "/home/$APPLICATION_USER" --create-home --shell /bin/bash --no-user-group "$APPLICATION_GROUP" -g "$APPLICATION_GID"
-
-echo "Welcome to Șerpărie!"
