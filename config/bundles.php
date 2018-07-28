5b3f7ddf7fdcc7472099d64b98c442a752970a59
Gabi Udrescu <gabiudrescu@users.noreply.github.com>
2018-07-28 17:25:46 +0300

    Expiry (#16)
    * remove lexik + fix translations for expire
    
    * fix stuff related to translations
    
    * fix wrong refactor
    
    * add another fix

--- config/bundles.php
+++ config/bundles.php
@@ -18,7 +18,6 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
-    Lexik\Bundle\TranslationBundle\LexikTranslationBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
--- src/EventSubscriber/JobSubscriber.php
+++ src/EventSubscriber/JobSubscriber.php
@@ -9,32 +9,84 @@ use Doctrine\ORM\Event\LifecycleEventArgs;
 
 class JobSubscriber implements EventSubscriber
 {
+    /**
+     * @var Slugify $slugify
+     */
+    private $slugify;
 
     public function __construct(Slugify $slugify)
     {
         $this->slugify = $slugify;
     }
 
+    /**
+     * @param LifecycleEventArgs $args
+     *
+     * @return Job|false
+     */
+    private function retrieveJob(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if($entity instanceof Job)
+        {
+            return $entity;
+        }
+
+        return false;
+    }
+
     public function prePersist(LifecycleEventArgs $args)
     {
-        $this->index($args);
+        if(!($job = $this->retrieveJob($args)))
+        {
+            return;
+        }
+
+        $this->slugifyJob($job);
+
+        $this->setCreatedAt($job);
+        $this->setUpdatedAt($job);
+        $this->setExpiresAt($job);
     }
 
     public function preUpdate(LifecycleEventArgs $args)
     {
-        $this->index($args);
+        if(!($job = $this->retrieveJob($args)))
+        {
+            return;
+        }
+
+        $this->slugifyJob($job);
+
+        $this->setUpdatedAt($job);
     }
 
-    private function index(LifecycleEventArgs $args)
+    private function slugifyJob(Job $job)
     {
-        $entity = $args->getEntity();
+        $job->setCompanySlug($this->slugify->slugify($job->getCompany()));
+        $job->setPositionSlug($this->slugify->slugify($job->getPosition()));
+        $job->setLocationSlug($this->slugify->slugify($job->getLocation()));
+    }
 
-        if ($entity instanceof Job){
-            $slugify = new Slugify();
+    private function setCreatedAt(Job $job)
+    {
+        if(!$job->getCreatedAt())
+        {
+            $job->setCreatedAt(new \DateTime());
+        }
+    }
 
-            $entity->setCompanySlug($slugify->slugify($entity->getCompany()));
-            $entity->setPositionSlug($slugify->slugify($entity->getPosition()));
-            $entity->setLocationSlug($slugify->slugify($entity->getLocation()));
+    private function setUpdatedAt(Job $job)
+    {
+        $job->setUpdatedAt(new \DateTime());
+    }
+
+    private function setExpiresAt(Job $job)
+    {
+        if (!$job->getExpiresAt()) {
+            $now = $job->getCreatedAt() ? $job->getCreatedAt()->format('U') : time();
+            $job->setExpiresAt(new \DateTime(date('Y-m-d H:i:s', $now + 86400 * 30)));
         }
     }
 
